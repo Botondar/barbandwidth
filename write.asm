240a61743646971104a215b0c3330ffea2b62911
Botondar <m.botond7@gmail.com>
2024-05-03 12:05:53 +0200

    Added unrolled writes (also fixed Write naming)

--- write.asm
+++ write.asm
@@ -1,13 +1,39 @@
-global Write16x1
+global Write32x1
+global Write32x2
+global Write32x4
 
 section .text
 
-Write16x1:
+Write32x1:
     vxorps ymm0, ymm0
     align 64
 .loop:
     vmovups [rdx], ymm0
     add rdx, 32
     sub rcx, 32
+    jnz .loop
+    ret
+
+Write32x2:
+    vxorps ymm0, ymm0
+    align 64
+.loop:
+    vmovups [rdx], ymm0
+    vmovups [rdx + 32], ymm0
+    add rdx, 64
+    sub rcx, 64
+    jnz .loop
+    ret
+
+Write32x4:
+    vxorps ymm0, ymm0
+    align 64
+.loop:
+    vmovups [rdx], ymm0
+    vmovups [rdx + 32], ymm0
+    vmovups [rdx + 64], ymm0
+    vmovups [rdx + 96], ymm0
+    add rdx, 128
+    sub rcx, 128
     jnz .loop
     ret
